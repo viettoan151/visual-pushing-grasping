31a8d8aab5dfdde8a64fcabda1c12a2c463a0b7f
viettoan151 <viettoan151@gmail.com>
2018-10-23 16:18:25 +0800

    countdown time for capture image

--- real/capture.py
+++ real/capture.py
@@ -6,11 +6,17 @@ from camera import Camera
 
 camera = Camera()
 time.sleep(1) # Give camera some time to load data
-
+count = 0
+delaytime = 5 #seconds delay
 while True:
+    print('Take picture {}'.format(count))
     color_img, depth_img = camera.get_data()
     plt.subplot(211)
     plt.imshow(color_img)
     plt.subplot(212)
     plt.imshow(depth_img)
     plt.show()
+    for i in range(delaytime):
+        print('{}'.format(delaytime - i))
+        time.sleep(1)
+    count = count + 1
